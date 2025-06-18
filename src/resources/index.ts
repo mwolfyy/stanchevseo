bf0b1220e34a6b5ea528ab81557e8db377db74b9
Stanchev <59915249+mwolfyy@users.noreply.github.com>
2025-06-18 11:27:32 +0300

    Update index.ts

--- src/resources/index.ts
+++ src/resources/index.ts
@@ -10,6 +10,7 @@ export {
   seoServices,
   contact,
   gallery,
+  handshake,
 } from "./content";
 
 export { display, mailchimp, routes, protectedRoutes, baseURL, fonts, style, schema, sameAs, effects, dataStyle, } from "./once-ui.config";
