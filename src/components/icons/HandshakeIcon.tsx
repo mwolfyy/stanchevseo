46beb4b421b503cb5c89d99551f4a824120d0b19
Stanchev <59915249+mwolfyy@users.noreply.github.com>
2025-06-18 12:59:05 +0300

    Create HandshakeIcon.tsx

---
+++ src/components/icons/HandshakeIcon.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const HandshakeIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    fill="none"
+    strokeWidth={2}
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
+    <path d="M18 11h2a2 2 0 0 1 2 2v2a2 ... (пълният `d` атрибут от сайта)" />
+  </svg>
+);
