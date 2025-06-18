df62756b1901ed21370b37c76612e29275bd2b84
Stanchev <59915249+mwolfyy@users.noreply.github.com>
2025-06-18 14:03:04 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export async function generateMetadata() {
       card: 'summary_large_image',
       title: home.title,
       description: home.description,
-      images: [https://stanchevseo.netlify.app/images/og/home.jpg],
+      images: ["https://stanchevseo.netlify.app/images/og/home.jpg"],
     },
   };
 }
