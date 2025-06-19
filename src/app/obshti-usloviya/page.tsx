aacd8df9f95b1abe529927397e936bfe77b7492c
Stanchev <59915249+mwolfyy@users.noreply.github.com>
2025-06-19 16:45:42 +0300

    Update page.tsx

--- src/app/obshti-usloviya/page.tsx
+++ src/app/obshti-usloviya/page.tsx
@@ -1,9 +1,11 @@
+'use client';
+
 import { Column, Flex, Feedback } from "@once-ui-system/core";
 
 export default function TermsPage() {
   return (
     <Column as="section" padding="xl" gap="xl" fillWidth>
-      <Flex as="h1" textVariant="display-default-m" horizontal="center">
+      <Flex as="h1" textVariant="display-strong-m" horizontal="center">
         Общи условия за ползване на услугите на Stanchev.bg
       </Flex>
 
@@ -12,43 +14,48 @@ export default function TermsPage() {
       </Flex>
 
       <Column gap="l">
+        {/* 1. Данни за доставчика */}
         <Flex as="h2" textVariant="display-default-m">
           1. Данни за доставчика
         </Flex>
         <Flex as="p">Stanchev.bg е уебсайт, собственост на Стоян Станчев.</Flex>
-        <Flex as="ul" listStyle="disc" paddingLeft="l">
+        <Column as="ul" gap="s" paddingLeft="l">
           <li>Уебсайт: https://stanchev.bg</li>
           <li>Имейл: seo@stanchev.bg</li>
-        </Flex>
+        </Column>
 
+        {/* 2. Обхват на услугите */}
         <Flex as="h2" textVariant="display-default-m">
           2. Обхват на услугите
         </Flex>
-        <Flex as="ul" listStyle="disc" paddingLeft="l">
+        <Column as="ul" gap="s" paddingLeft="l">
           <li>SEO одит и анализ</li>
           <li>On-page оптимизация</li>
           <li>Keyword research</li>
           <li>SEO консултации</li>
           <li>Оптимизация на съдържание</li>
-        </Flex>
-        <Callout variant="warning">
-          Услугите се предоставят след потвърждение и писмено съгласие между двете страни.
-        </Callout>
+        </Column>
+        <Feedback
+          title="Важно"
+          description="Услугите се предоставят след потвърждение и писмено съгласие между двете страни."
+        />
 
+        {/* 3. Права и отговорности */}
         <Flex as="h2" textVariant="display-default-m">
           3. Права и отговорности
         </Flex>
         <Flex as="p">Потребителят се задължава:</Flex>
-        <Flex as="ul" listStyle="disc" paddingLeft="l">
+        <Column as="ul" gap="s" paddingLeft="l">
           <li>Да предоставя вярна и актуална информация</li>
           <li>Да използва услугите по добросъвестен начин</li>
-        </Flex>
+        </Column>
         <Flex as="p">Stanchev.bg си запазва правото:</Flex>
-        <Flex as="ul" listStyle="disc" paddingLeft="l">
+        <Column as="ul" gap="s" paddingLeft="l">
           <li>Да променя цените и съдържанието на сайта без предизвестие</li>
           <li>Да откаже услуга при съмнение за недобросъвестност</li>
-        </Flex>
+        </Column>
 
+        {/* 4. Поверителност */}
         <Flex as="h2" textVariant="display-default-m">
           4. Поверителност
         </Flex>
@@ -56,16 +63,19 @@ export default function TermsPage() {
           Всички данни и анализи, предоставени от клиента, се третират като строго поверителни и не се споделят с трети страни.
         </Flex>
 
+        {/* 5. Плащания и срокове */}
         <Flex as="h2" textVariant="display-default-m">
           5. Плащания и срокове
         </Flex>
         <Flex as="p">
           Услугите се заплащат предварително, освен ако не е договорено друго. Изпълнението започва след потвърдено плащане.
         </Flex>
-		<Feedback
-		title="Важно" 
-		description="За абонаментни услуги и дългосрочни партньорства се договарят индивидуални условия."
-		/>
+        <Feedback
+          title="Важно"
+          description="За абонаментни услуги и дългосрочни партньорства се договарят индивидуални условия."
+        />
+
+        {/* 6. Авторски права */}
         <Flex as="h2" textVariant="display-default-m">
           6. Авторски права
         </Flex>
@@ -73,6 +83,7 @@ export default function TermsPage() {
           Всички материали на сайта (текстове, графики, дизайн) са собственост на Стоян Станчев и не могат да бъдат използвани без разрешение.
         </Flex>
 
+        {/* 7. Заключителни разпоредби */}
         <Flex as="h2" textVariant="display-default-m">
           7. Заключителни разпоредби
         </Flex>
