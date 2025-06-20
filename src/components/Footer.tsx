87f0cd9e511d6d0735e5eb2dea4f3404953730ef
Stanchev <59915249+mwolfyy@users.noreply.github.com>
2025-06-20 15:13:00 +0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -24,8 +24,7 @@ export const Footer = () => {
       >
         <Text variant="body-default-s" onBackground="neutral-strong">
           <Text onBackground="neutral-weak">© {currentYear} /</Text>
-          <Text paddingX="4">{person.name}</Text>
-          <Text paddingX="4">{person.name}</Text>
+          <Text paddingX="4">{person.name} /</Text>
           <a href="/obshti-usloviya">Общи условия</a>
         </Text>
         <Flex gap="16">
